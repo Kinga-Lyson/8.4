3650351e594d7d6842544178d14b5e141d140f3a
Kinga Łysoń <kingalyson8@gmail.com>
2017-10-30 09:17:38 +0100

    add new project 8.4

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+/*zadeklarowanie funkcji name i wywołanie funkcji prompt */
+var name = prompt('Enter your name');
+// wywołanie funkcji alert z parametrem, wyświetlenie wartości w okienku przeglądarki
+alert('Hello, ' + name);
+/*wartość wyświetlona w konsoli*/
+console.log('Hello, ' + name);
